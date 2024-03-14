8776509b1d046fe8f978932b73c5f9b451c9f706
Luiz Pascucho <luizpascucho@bol.com.br>
2024-03-14 18:39:14 -0300

    atualizacoes_model

---
+++ src/ecommerce/model/EcommerceModel.java
@@ -0,0 +1,30 @@
+package ecommerce.model;
+
+public class EcommerceModel {
+
+	private String nome;
+	private int ano;
+	private String genero;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	
+	
+}
---
+++ src/ecommerce/repository/EcommerceRepository.java
@@ -0,0 +1,15 @@
+package ecommerce.repository;
+
+
+public interface EcommerceRepository {
+
+	public void listarTodos();
+	public void adicionar();
+	public void atualizar();
+	public void apagar();
+	
+		
+}
+
+	
+
